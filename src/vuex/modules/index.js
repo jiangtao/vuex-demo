e747f012dae3db03337f1dc7c4fb54880dc15f76
Jerret321 <321jiangtao@gmail.com>
2017-08-14 16:50:56 +0800

    default use namespaced true

--- src/vuex/modules/index.js
+++ src/vuex/modules/index.js
@@ -1,5 +1,21 @@
 // 拆解 store 
 
 export default {
-  
+  product: {
+    // use namespaced to support multiple namespace find 
+    // core: vuex/src/store.js installModule function 
+    namespaced: true, 
+    state: {
+      
+    },
+    getters: {
+      
+    },
+    actions: {
+      
+    },
+    mutations: {
+      
+    }
+  }
 }
